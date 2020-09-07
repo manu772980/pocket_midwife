40933c49e3ef97f3e8c1b3664f9c4bb1bbc464ef
goutham1389 <70475526+goutham1389@users.noreply.github.com>
2020-09-07 11:55:09 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ function clickFunction(){
 	console.log("click works!!!")
 }
 
+function searchBar(){
+	var tagList = ["h3", "p", "a", "h1", "h2"];
+	$('#nav_searchbar_btn').click(
+		// if someone clicks the button
+		function(){
+			var input = $("#nav_searchbar_inp").val() //Takes the textbox textmy search text
+			$( tagList ).each(function() {
+  				$(this + ":contains('" + input + "')").css("background-color", "yellow");
+			});
+		}
+	);
+}
+function function_name(argument) {
+	$('#nav_searchbar_form').submit(function () {
+ 		searchBar();
+ 		return false;
+	});
+}
+
+
 $(document).ready(function() {
       console.log("Howdy!!!");
       clickFunction();
+      searchBar();
 });
