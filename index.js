6730a5c8f4452c53ec884298d98960c427840ba2
Naveen-Nekkanti <69735339+Naveen-Nekkanti@users.noreply.github.com>
2020-10-01 19:04:44 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 
 function clickFunction(){
-	console.log("click works!!!")
+	console.log("click works!!!");
 }
 
+function homeClickFunction(){
+	console.log("This is home option");
+}
+function myStoryClickFunction(){
+	console.log("This is my story option");
+}
+function vlogClickFunction(){
+	console.log("This is vlog option");
+}
+function storeClickFunction(){
+	console.log("This is store option");
+}
+function contactClickFunction(){
+	console.log("This is contact option");
+}
+function prepForBirthClickFunction(){
+	console.log("This is buy now for preparing for birth option");
+}
+function birthCompanionClickFunction(){
+	console.log("This is buy now for birth companion option");
+}
+function secretWomenBusinessClickFunction(){
+	console.log("This is click here for secret women's business option");
+}
+
+//security architect
+function checkCharacter(str){
+	if(/^[a-zA-Z0-9- ]*$/.test(str) == false) {
+    	console.warn('Your search string contains illegal characters.');
+    	return false;
+	}
+}
+//security architect
+
 function searchBar(){
-	var tagList = ["h3", "p", "a", "h1", "h2"];
+	var tagList = ["h3", "h4", "p", "a", "h1", "h2"];
 	$('#nav_searchbar_btn').click(
 		// if someone clicks the button
 		function(){
 			var input = $("#nav_searchbar_inp").val() //Takes the textbox textmy search text
+			checkCharacter(input);
 			$( tagList ).each(function() {
   				$(this + ":contains('" + input + "')").css("background-color", "yellow");
 			});
@@ -22,9 +57,22 @@ function function_name(argument) {
 	});
 }
 
+function preventEnterKeyResponse(){
+	$('#nav_searchbar_form').keypress(function(e) { 
+    	return e.keyCode != 13;
+	});
+}
+
+function create_menu(){
+	$("#nav-bar-menu").append('<li class="nav-item" onClick="homeClickFunction()"><a class="nav-link" href="#">HOME</a></li>'
+	+'<li class="nav-item" onClick="myStoryClickFunction()"><a class="nav-link" href="#">MY STORY</a></li>'
+	+'<li class="nav-item" onClick="vlogClickFunction()"><a class="nav-link" href="#">VLOG</a></li>'
+	+'<li class="nav-item" onClick="storeClickFunction()"><a class="nav-link" href="#">STORE</a></li>'
+	+'<li class="nav-item" onClick="contactClickFunction()"><a class="nav-link" href="#">CONTACT</a></li>');
+}
 
 $(document).ready(function() {
-      console.log("Howdy!!!");
-      clickFunction();
       searchBar();
+      create_menu();
+      preventEnterKeyResponse();
 });
